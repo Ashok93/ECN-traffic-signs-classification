529bff9fde52dc2a5bf2032f6aef891f5b1ff94d
Ashok M <ashok.kannan93@gmail.com>
2018-04-23 21:07:49 +0200

    Added source directory. Building initial structure of project.

---
+++ src/traffic_signs_classifier.py
@@ -0,0 +1,13 @@
+## imports #################################################
+
+import numpy as np # scientific computations library (http://www.numpy.org/)
+import tensorflow as tf # deep learning library (https://www.tensorflow.org/)
+import cv2 as cv # OpenCV computer vision library (https://opencv.org/)
+import matplotlib.pyplot as plt # Plotting library (https://matplotlib.org/)
+import pickle # library for saving and retrieving trained model (https://docs.python.org/2/library/pickle.html)
+
+import os # file operations
+import math
+
+#############################################################
+
